86d2f712f59bc79f389c3a54e0b5a1976c909feb
mln00b <darkdaysareback@gmail.com>
2021-09-25 01:21:31 +0530

    test case fix

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ import os
 import csv
 
 def test_data_not_uploaded():
-    assert os.path.isdir("data"), "Data exists. This should not be uploaded"
+    assert not os.path.isdir("data"), "Data exists. This should not be uploaded"
 
 def test_model_not_uploaded():
-    assert os.path.isfile("model.pth"), "Model exists. This should not be uploaded"
+    assert not os.path.isfile("model.pth"), "Model exists. This should not be uploaded"
 
 def test_accuracy():
     if os.path.isfile("Metrics.csv"):
         with open("Metrics.csv") as f:
             cr = csv.reader(f)
             last = list(cr)[-1]
-            val_acc = last[3]
+            val_acc = float(last[3])
             assert val_acc > 0.7, f"Val accuracy < 70%: {val_acc}"
 
 def test_per_class_accuracy():
@@ -20,7 +20,7 @@ def test_per_class_accuracy():
         with open("Metrics.csv") as f:
             cr = csv.reader(f)
             last = list(cr)[-1]
-            class_1 = last[-2]
-            class_2 = last[-1]
+            class_1 = float(last[-2])
+            class_2 = float(last[-1])
             assert class_1 > 0.7, f"Class 1 accuracy < 70%: {class_1}"
             assert class_2 > 0.7, f"Class 1 accuracy < 70%: {class_2}"
